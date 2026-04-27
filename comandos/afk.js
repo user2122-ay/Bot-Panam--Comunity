5c4a36effde19030c2bd207b5efd6ceeffb82690
user2122-ay <anthonybatista2010@gmail.com>
2026-04-26 22:15:36 -0500

    Update afk.js

--- comandos/afk.js
+++ comandos/afk.js
@@ -1,25 +1,30 @@
+const { SlashCommandBuilder } = require('discord.js');
+
 const afkUsers = new Map();
 
 module.exports = {
-  name: 'afk',
+  data: new SlashCommandBuilder()
+    .setName('afk')
+    .setDescription('Ponerte AFK')
+    .addStringOption(option =>
+      option.setName('razon')
+        .setDescription('Razón del AFK')
+        .setRequired(false)
+    ),
 
-  async execute(message, args) {
-    const razon = args.join(' ') || 'Sin razón';
+  async execute(interaction) {
+    const razon = interaction.options.getString('razon') || 'Sin razón';
 
-    // Guardar tiempo y razón
-    afkUsers.set(message.author.id, {
-      razon: razon,
+    afkUsers.set(interaction.user.id, {
+      razon,
       tiempo: Date.now()
     });
 
-    // Cambiar nickname
     try {
-      await message.member.setNickname(`[AFK] ${message.member.displayName}`);
-    } catch (e) {
-      console.log('No se pudo cambiar el nombre');
-    }
+      await interaction.member.setNickname(`[AFK] ${interaction.member.displayName}`);
+    } catch {}
 
-    message.reply(`😴 Ahora estás AFK: ${razon}`);
+    await interaction.reply(`😴 Estás AFK: ${razon}`);
   },
 
   afkUsers
